b54c1a070996ddefa2b47e7a3ad42b93eb1e469d
synty4 <syntshimbi4@gmail.com>
2017-06-27 14:25:54 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+import java.awt.Label;
+
+public class Test extends java.applet.Applet {
+   public void init (){
+      add(new Label("Hello World"));
+   }
+}
---
+++ tests/fail/MinusAssign.java
@@ -0,0 +1,13 @@
+package fail ;
+
+import java.lang.System ;
+
+public class MinusAssign {
+
+	public static void main ( String [] args ) {
+
+		System . out . println ('a' -= 2);
+
+	}
+
+} 
